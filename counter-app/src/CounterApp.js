fd566c00bad3d159c7a8fe59bc62129897797e20
Carlos <crdgom@gmail.com>
2021-07-21 20:56:21 -0500

    Added CounterApp test for action buttons

--- counter-app/src/CounterApp.js
+++ counter-app/src/CounterApp.js
@@ -19,8 +19,8 @@ const CounterApp = ({name, value}) => {
             <h1>{name}</h1>
             <h2>{counter}</h2>
             <button onClick={handleAdd}>+1</button>
-            <button onClick={handleRemove}>-1</button>
             <button onClick={handleReset}>Reset</button>
+            <button onClick={handleRemove}>-1</button>
         </>
     );
 }
--- counter-app/src/unitary-tests/CounterApp.test.js
+++ counter-app/src/unitary-tests/CounterApp.test.js
@@ -5,6 +5,11 @@ import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 describe('Testing for CounterApp component',()=>{
+    let wrapper = wrapper = shallow(<CounterApp />);
+    beforeEach(()=>{
+        wrapper = shallow(<CounterApp />);
+    })
+
     test('Must show default value defined in CounterApp component',()=>{
         const value = 100;
         const wrapper = shallow(<CounterApp value={value}/>);
@@ -18,5 +23,33 @@ describe('Testing for CounterApp component',()=>{
             />)
         const heading2 = wrapper.find('h2').text() 
         expect(heading2).toBe(value);
+    })
+
+})
+
+describe('Testing for CounterApp buttons (plus 1, reset & minus 1)',()=>{
+    const wrapper = shallow(<CounterApp />);
+    test('Should simulate the behavior of the button plus one (+1) ',()=>{
+        
+        const plusOne = wrapper.find('button').at(0).simulate('click');
+        const heading2 = wrapper.find('h2').text()
+        expect(heading2).toBe('101')
+
+    })
+    test('Should simulate the behavior of the button reset (Reset) ',()=>{
+        
+        const reset = wrapper.find('button').at(1).simulate('click');
+        const heading2 = wrapper.find('h2').text()
+        expect(heading2).toBe('100')
+        
+
+
+    })
+    test('Should simulate the behavior of the button minus one (+1) ',()=>{
+        
+        const minusOne = wrapper.find('button').at(2).simulate('click');
+        const heading2 = wrapper.find('h2').text()
+        expect(heading2).toBe('99')
+
     })
 })
